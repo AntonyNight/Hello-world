e5b0014df49c4713c4e0ec2aad4c305ec997042f
admin <avs82@inbox.ru>
2024-02-20 13:11:46 +0500

    Первый коммит branch

--- lesson6/src/Main2.java
+++ lesson6/src/Main2.java
@@ -2,5 +2,6 @@ public class Main2 {
     public static void main(String[] args) {
         System.out.println("143");
         System.out.println("1445453");
+        System.out.println("1new_branch1");
     }
 }
